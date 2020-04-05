062a9e97dd2965117c995bf1a4160e770932b357
Romain Yon <yonromai@users.noreply.github.com>
2020-04-05 19:49:17 -0400

    turn on TravisCI

--- snooze/main_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-dummy_py_txt = """
-# snooze: 1900-01-01
-lib.broken_logic = my_fixed_logic # FIXME: remove when lib==0.42 gets released
-"""
-
-def test_foo() -> None:
-    assert False
