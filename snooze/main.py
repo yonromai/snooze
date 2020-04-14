bd851569680b8d61e0d06c9b424e94b73cac243c
Romain Yon <yonromai@users.noreply.github.com>
2020-04-13 20:24:02 -0400

    refactor main/cli logic

--- snooze/main.py
+++ snooze/main.py
@@ -5,31 +5,14 @@ import os
 import sys
 from datetime import datetime
 from pathlib import Path
+from typing import List
 
 from dateutil.parser import parse
 
 from snooze.parser import DEFAULT_WOKERS, SnoozeParser
 
 
-def main(directory: Path, time: datetime, processes: int) -> None:
-    if not directory.exists():
-        print(f"ERROR: Could not find directory: {directory}")
-        sys.exit(os.EX_USAGE)
-
-    print(f'Searching for snoozes in "{directory}"')
-    found_matches = False
-
-    for match in SnoozeParser.search_all_files(root_dir=directory, now=time):
-        found_matches = True
-        print(match)
-
-    if found_matches:
-        sys.exit(os.EX_DATAERR)
-    print("No triggered snooze found.")
-    sys.exit(os.EX_OK)
-
-
-if __name__ == "__main__":
+def parse_args(argv: List[str]) -> argparse.Namespace:  # FIXME
     parser = argparse.ArgumentParser(
         description="Leave reminders in your code. Useful to set a timer on FIXMEs, TODOs & scheduled releases."
     )
@@ -52,9 +35,33 @@ if __name__ == "__main__":
         type=int,
         help=f"Number of threads used to parse files (default: {DEFAULT_WOKERS})",
     )
-    args = parser.parse_args()
-    main(
-        Path(args.directory),
-        time=parse(args.time) if args.time else datetime.utcnow(),
-        processes=args.threads,
-    )
+    assert len(argv) >= 1, "args should start with the program executable"
+    return parser.parse_args(args=argv[1:])
+
+
+def main(argv: List[str] = sys.argv) -> None:
+    args = parse_args(argv)
+    directory = Path(args.directory)
+    time = parse(args.time) if args.time else datetime.utcnow()
+    processes = args.threads
+    if not directory.exists():
+        print(f"ERROR: Could not find directory: {directory}")
+        sys.exit(os.EX_USAGE)
+
+    print(f'Searching for snoozes in "{directory}"')
+    found_matches = False
+
+    for match in SnoozeParser.search_all_files(
+        root_dir=directory, now=time, processes=processes
+    ):
+        found_matches = True
+        print(match)
+
+    if found_matches:
+        sys.exit(os.EX_DATAERR)
+    print("No triggered snooze found.")
+    sys.exit(os.EX_OK)
+
+
+if __name__ == "__main__":
+    main()
